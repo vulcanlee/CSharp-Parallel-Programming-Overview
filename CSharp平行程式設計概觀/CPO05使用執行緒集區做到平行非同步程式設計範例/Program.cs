34ee480da85e85c77bd17f9115b7c1afc00bea20
Vulcan <vulcan.lee@gmail.com>
2021-02-10 21:40:59 +0800

    CPO05|使用執行緒集區做到平行非同步程式設計範例

---
+++ CSharp平行程式設計概觀/CPO05使用執行緒集區做到平行非同步程式設計範例/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace CPO05使用執行緒集區做到平行非同步程式設計範例
+{
+    /// <summary>
+    /// 說明 ： 使用執行緒集區做到平行Parallelism非同步 asynchronous 方式分批計算不同區間的所有質數數量
+    /// 備註 ： 開始執行前，請打開工作管理員，觀察處理器使用效能趨勢圖
+    ///        透過執行緒功能，做到非同步 CPU Bound 集中的處理能力
+    ///        使用 Release 方案組態來進行建置與執行
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 請根據本身電腦，調整成為適當的大小
+            int lastNumber = 20000000;
+            int partition = 4;
+            int part = lastNumber / partition;
+            List<(int begin, int end)> range = new List<(int begin, int end)>();
+            // 代表執行緒同步處理事件會在發出訊號時，釋出一個等候執行緒之後，就自動重設
+            List<AutoResetEvent> autoResetEvents = new List<AutoResetEvent>();
+            for (int i = 1; i <= partition; i++)
+            {
+                int begin = part * (i - 1) + 1;
+                int end = part * i;
+                if (begin < 2) begin = 2;
+                if (i == partition) end = lastNumber;
+                range.Add((begin, end));
+                autoResetEvents.Add(new AutoResetEvent(false));
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            #region 找出所有的質數
+            ConcurrentBag<List<int>> allPrimes = new ConcurrentBag<List<int>>();
+            for (int i = 0; i < partition; i++)
+            {
+                int idx = i;
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    allPrimes.Add(ComputeAllPrimeNumbers
+                        (range[idx].begin, range[idx].end));
+                    autoResetEvents[idx].Set();
+                });
+            }
+
+            EventWaitHandle.WaitAll(autoResetEvents.ToArray());
+            #endregion
+
+            stopwatch.Stop();
+
+            allPrimes.Sum(x => x.Count());
+            Console.WriteLine("Primes : {0}\nTime: {1}",
+                allPrimes.Sum(x => x.Count()), stopwatch.ElapsedMilliseconds);
+        }
+
+        #region 找出兩數值間的所有質數
+        static List<int> ComputeAllPrimeNumbers(int min, int max)
+        {
+            List<int> result = new List<int>();
+            for (int i = min; i <= max; i++)
+            {
+                if (IsPrime(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        #region 判斷指定數值是否為質數
+        static bool IsPrime(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return n == 2;
+            }
+            else
+            {
+                var topLimit = (int)Math.Sqrt(n);
+                for (int i = 3; i <= topLimit; i += 2)
+                {
+                    if (n % i == 0) return false;
+                }
+                return true;
+            }
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 |CPO02|切割成為多區塊的同步Synchronous程式設計範例||
 |CPO03|切割成為多區塊的平行Parallelism非同步Ansynchronous程式設計範例||
 |CPO04|並行Concurrency的非同步Ansynchronous程式設計範例||
-|CPO05|||
+|CPO05|使用執行緒集區做到平行非同步程式設計範例||
 |CPO06|||
 |CPO07|||
 |CPO08|||
