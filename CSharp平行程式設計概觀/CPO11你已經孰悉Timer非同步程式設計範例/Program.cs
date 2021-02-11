c7df8ff751185d7caccd4f4cf52a1d3f38105ecb
Vulcan <vulcan.lee@gmail.com>
2021-02-11 20:29:14 +0800

    CPO11|你已經孰悉Timer非同步程式設計範例

---
+++ CSharp平行程式設計概觀/CPO11你已經孰悉Timer非同步程式設計範例/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+
+namespace CPO11你已經孰悉Timer非同步程式設計範例
+{
+    /// <summary>
+    /// 說明 ： 使用 Timer 類別做到非同步方面的應用開發
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 準備 Timer 物件
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 5000;
+            timer.AutoReset = false;
+            timer.Elapsed += Timer_Elapsed;
+            #endregion
+
+            Console.WriteLine($"[我] : 老闆我要一碗麵");
+            Console.WriteLine($"[老闆] : 沒問題，5秒後就會好");
+            Console.WriteLine($"[我] : 好，那我等下過來拿");
+            timer.Start();
+            for (int i = 0; i < 6; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"[我] : 正在逛街中...");
+            }
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"[老闆] : 客人，你的麵做好了");
+        }
+    }
+}
